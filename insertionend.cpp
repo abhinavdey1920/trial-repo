7521d1edd56f2c2f856ceb24ec0fc36f50f864ad
ABHINAV DEY <75990650+abhinavdey1920@users.noreply.github.com>
2021-08-16 18:36:11 +0530

    Add files via upload

---
+++ insertionend.cpp
@@ -0,0 +1,44 @@
+//code by abhinav dey
+#include<iostream>
+using namespace std;
+struct Node{
+	int data;
+	Node* next;
+	Node(int x){
+		data=x;
+		next=NULL;
+	}
+};
+Node* insertend(Node* head,int x){
+	Node* temp=new Node(x);
+	if(head==NULL){
+		return temp;
+	}
+	Node* curr=head;
+	while(curr->next!=NULL){
+		curr=curr->next;
+	}
+	curr->next=temp;
+	cout<<"y5uy "<<head->data<<" "<<endl;
+	return head;
+}
+void print(Node* head){
+	Node* curr=head;
+	while(curr!=NULL){
+		cout<<curr->data<<" ";
+		curr=curr->next;
+	}
+}
+int main(){
+	Node* head=NULL;
+	head=insertend(head,20);
+	cout<<head->data<<" ";
+	head=insertend(head,30);
+	cout<<head->data;
+	head=insertend(head,40);
+	cout<<head->data;
+	head=insertend(head,50);
+	cout<<head->data;
+	print(head);
+	
+}
